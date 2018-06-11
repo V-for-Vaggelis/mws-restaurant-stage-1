0a11e0f560a82b88cfedd10b7ffdabff0c6a7464
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-06-11 20:15:45 +0300

    Made service worker show something offline if home page loaded before

---
+++ sw.js
@@ -0,0 +1,38 @@
+const cacheName = "restaurant-reviews-v1.0";
+const urlsToCache = [
+  '/',
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js'
+];
+
+// This will cache all of our files, once installing the sw is done
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// This will intercept any fetch requests, and if they've been made before, it will respond from the cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {return response;}
+      else {return fetch(event.request);}
+    })
+  );
+});
