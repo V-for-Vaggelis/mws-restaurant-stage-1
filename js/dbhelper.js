b955d5900775842078e383b2edcf9d58955f96ef
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-06-13 17:22:41 +0300

    Removed last breakpoint

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,7 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 2018 // Change this to your server port
+    const port = 2019 // Change this to your server port
     return `http://localhost:${port}/data/restaurants.json`;
     // return 'https://github.com/V-for-Vaggelis/mws-restaurant-stage-1/tree/master/data/restaurants.json';
   }
